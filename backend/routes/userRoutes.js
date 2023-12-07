963a423e4d34cbbc7adf3bf7b3fed75dc6c26025
irfan-flutions <irfan@flutions.in>
2023-12-07 17:17:03 +0530

    updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,8 @@
-const express=require('express');
-const {registerUser,authUser}=require('../controllers/userControllers')
-const router=express.Router();
+const express = require('express');
+const { registerUser, authUser } = require('../controllers/userControllers');
+const router = express.Router();
 
+router.route('/').post(registerUser);
+router.route('/login').post(authUser);
 
-router.route('/').post(registerUser)
-router.route('/login').post(authUser)
-
-module.exports=router;
+module.exports = router;
