ca508af26246d74c85ba60852b7e986d16a85970
irfan-flutions <irfan@flutions.in>
2023-12-08 10:57:16 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -21,16 +21,6 @@ app.use((req, res, next) => {
     next(error);
   });
 
-  // Error handling middleware
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-      message: err.message,
-      stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
-    });
-  });
-
 app.use(notFound);
 app.use(errorHandler);
 
