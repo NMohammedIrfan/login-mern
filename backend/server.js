51c45f7abe0437dd8596620028991822287576c1
irfan-flutions <irfan@flutions.in>
2023-12-20 18:34:27 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 const app = express();
-const userRoutes=require('./routes/userRoutes');
-const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
-const cors = require('cors');
-
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users',userRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage");
+});
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-  });
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
